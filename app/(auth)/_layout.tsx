f0b9183dfd89a5edd504c67c3740f56dd8058ec3
MyProfilCookie <parigopsg24@gmail.com>
2026-05-01 21:08:23 +0200

    feat: project setup, nativewind, routing, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,7 +3,7 @@ import "../../global.css";
 
 export default function AuthLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
     </Stack>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView)
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
@@ -12,8 +15,7 @@ export default function App() {
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary p-4 text-white">Se connecter</Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary p-4 text-white">Créer un compte</Link>
       <Link href="/(tabs)/subscriptions" className="mt-4 rounded bg-primary p-4 text-white">Voir les abonnements</Link>
-      {/* <Link href="/(tabs)/subscriptions/1" className="mt-4 rounded bg-primary p-4 text-white">Voir un abonnement</Link> */}
       
-    </View>
+    </SafeAreaView>
   );
 }
