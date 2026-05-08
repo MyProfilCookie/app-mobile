903a2ecec8487f25644fb604afd94388aa19baa0
MyProfilCookie <parigopsg24@gmail.com>
2026-05-08 17:02:49 +0200

    feat: integrate custom fonts and update home screen UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,13 +8,12 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-7xl font-sans-extrabold">
+        Accueil
       </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary p-4 text-white">Aller sur le Dashboard</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary p-4 text-white">Se connecter</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary p-4 text-white">Créer un compte</Link>
-      <Link href="/(tabs)/subscriptions" className="mt-4 rounded bg-primary p-4 text-white">Voir les abonnements</Link>
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary p-4 text-white">Aller sur le Dashboard</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary p-4 text-white">Se connecter</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary p-4 text-white">Créer un compte</Link>
       
     </SafeAreaView>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,30 @@
-import { Stack } from "expo-router";
-import "../global.css";
+import "@/global.css";
+import { useFonts } from "expo-font";
+import { Stack, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+   'sans-regular': require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+   'sans-semibold': require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+   'sans-bold': require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+   'sans-extrabold': require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+   'sans-light': require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+   'sans-medium': require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }}>
     <Stack.Screen name="(auth)" />
     <Stack.Screen name="(tabs)" />
